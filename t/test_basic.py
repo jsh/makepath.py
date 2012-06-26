6c61022843d3f339aba179ebbb25b5b181ea0d62
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2012-06-25 19:36:20 -0600

    basic test structure

---
+++ t/test_basic.py
@@ -0,0 +1,12 @@
+class testBase(unittest.TestCase):
+   @classmethod
+   def setUp(self):
+      ###  code to do setup
+
+   def tearDown(self):
+      ###  code to do tear down
+
+   def test_Basic(self):
+      ###  A test case
+
+unittest.main()
